2565ee5e1107d845fad6a88ce4055939ab365439
benjamin <benjamin.ostendorf@gmx.de>
2024-02-06 15:09:37 +0100

    add binding for sex selection in GetPatientDataViewModel

--- ViewModels/GetPatientDataViewModel.cs
+++ ViewModels/GetPatientDataViewModel.cs
@@ -1,15 +1,26 @@
 using System.ComponentModel;
-using System.Runtime.CompilerServices;  
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ReactiveUI;
 namespace SecaFolderWatcher.ViewModels;
 
 public class GetPatientDataViewModel : ViewModelBase
 {
   //public event PropertyChangedEventHandler? PropertyChanged;
-  //public ICommand ClickOKCommand { get; }
+  private string _sex = "";
+  public string Sex
+  {
+    get => _sex;
+    set {
+      Logger.Log(value);
+      this.RaiseAndSetIfChanged(ref _sex, value);
+    }
+  }
 
   public void ClickOK()
   {
     Logger.LogInformation("Clicked Button OK");
+    Logger.LogInformation("currently selected sex is " + _sex);
   }
 
   public void ClickCancel()
