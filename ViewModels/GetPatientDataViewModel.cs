50f0f612ce4390ddf05e81cc8be6adc8d2914ffa
benjamin <benjamin.ostendorf@gmx.de>
2024-02-06 13:49:17 +0100

    add button components to GetPatientDataViewModel and view

--- ViewModels/GetPatientDataViewModel.cs
+++ ViewModels/GetPatientDataViewModel.cs
@@ -5,6 +5,17 @@ namespace SecaFolderWatcher.ViewModels;
 public class GetPatientDataViewModel : ViewModelBase
 {
   //public event PropertyChangedEventHandler? PropertyChanged;
+  //public ICommand ClickOKCommand { get; }
+
+  public void ClickOK()
+  {
+    Logger.LogInformation("Clicked Button OK");
+  }
+
+  public void ClickCancel()
+  {
+    Logger.LogInformation("Clicked Button Cancel");
+  }
 
   public GetPatientDataViewModel()
   {
