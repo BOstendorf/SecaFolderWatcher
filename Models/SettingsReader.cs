f38539298782e8713b7e7e813743f7e289742f19
benjamin <benjamin.ostendorf@gmx.de>
2024-02-06 17:43:26 +0100

    add function to find the SecaFolderWatcher.ini file in the executables parent directories

--- Models/SettingsReader.cs
+++ Models/SettingsReader.cs
@@ -7,12 +7,33 @@ namespace SecaFolderWatcher;
 public static class SettingsReader
 {
   private static bool _initialized = false;
+  private static string _executableDir = ""; 
+  private static string _iniFilePath;
   public static void InitSettingsReader() {
-    string directory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-    Logger.LogInformation($"executable path is ${directory}");
+    _executableDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+    Logger.LogInformation($"executable path is ${_executableDir}");
+    _iniFilePath = GetIniFilePath();
   }
 
   private static string GetIniFilePath(){
-    return "";
+    DirectoryInfo searchPath = Directory.GetParent(_executableDir);
+    DirectoryInfo previousPath = new DirectoryInfo(_executableDir);
+    while (!searchPath.Equals(previousPath))
+    {
+      if (searchPath.Name.Equals("SecaFolderWatcher"))
+      {
+        FileInfo[] directoryFiles = searchPath.GetFiles();
+        foreach (FileInfo file in directoryFiles)
+        {
+          if (file.Name.Equals("SecaFolderWatcher.ini"))
+          {
+            return file.FullName;
+          }
+        }
+      }
+      previousPath = searchPath;
+      searchPath = Directory.GetParent(searchPath.FullName);
+    }
+    throw new FileNotFoundException("There is no Path with SecaFolderWatcher/SecaFolderWatcher.ini in the executables parent directories");
   }
 }
