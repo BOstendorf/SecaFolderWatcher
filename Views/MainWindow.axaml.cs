11413a7cf5565c7c9f00f7d30d5f24927217c2a8
benjamin <benjamin.ostendorf@gmx.de>
2024-02-06 11:43:40 +0100

    add capability to show dialog window to mainwindow bind open main window call to NAKO button for testing

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,16 +1,29 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Diagnostics;
+using Avalonia.ReactiveUI;
+using SecaFolderWatcher.ViewModels;
+using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace SecaFolderWatcher.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(action => action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
     }
 
+    private async Task DoShowDialogAsync(InteractionContext<DialogWindowViewModel, DialogResultViewModel?> interaction)
+    {
+      var dialog = new DialogWindow();
+      dialog.DataContext = interaction.Input;
+
+      var result = await dialog.ShowDialog<DialogResultViewModel?>(this);
+      interaction.SetOutput(result);
+    }
 
     public void ButtonClick_HCHS(object source, RoutedEventArgs args)
     {
