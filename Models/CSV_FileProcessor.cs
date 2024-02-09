bd41e81cab7c53bee8732e93eea18d2fd4c621c0
benjamin <benjamin.ostendorf@gmx.de>
2024-02-09 16:27:33 +0100

    add guard function to ensure that a given path points to a .csv file and that the file exists

--- Models/CSV_FileProcessor.cs
+++ Models/CSV_FileProcessor.cs
@@ -1,5 +1,7 @@
 
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace SecaFolderWatcher;
@@ -7,6 +9,18 @@ namespace SecaFolderWatcher;
 public class CSV_FileProcessor
 {
   private static int _uniqueCounter = 0;
+
+  public static void EnsureCSV(FileInfo file)
+  {
+    if(!file.Extension.Equals(".csv"))
+    {
+      throw new ArgumentException($"The given path is expected to point to a .csv file. The actual path is {file.FullName}");
+    }
+    if(!File.Exists(file.FullName))
+    {
+      throw new FileNotFoundException($"The csv file at {file.FullName} could not be found");
+    }
+  }
 
   private static string AssignUniqueID(){
     string id = $"U_{_uniqueCounter}";
