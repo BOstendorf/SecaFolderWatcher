571f1d05fc7933f34fbf2bb52b220594eee40a1a
benjamin <benjamin.ostendorf@gmx.de>
2024-02-14 12:38:59 +0100

    add binding and callback for button clear that empties the info text field

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -40,6 +40,12 @@ namespace SecaFolderWatcher.ViewModels
             InfoText = InfoText + "test\n";
             Logger.LogInformation("Clicked Button HCHS");
         }
+
+        public void ButtonClearClick()
+        {
+          InfoText = "";
+        }
+
         public GetPatientDataViewModel PatientData { get; } = new GetPatientDataViewModel();
 
         public MainWindowViewModel()
