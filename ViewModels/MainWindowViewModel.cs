aefeee8058cef6dc3fc0cb1ee9462d5d368d16dc
benjamin <benjamin.ostendorf@gmx.de>
2024-02-14 11:33:07 +0100

    formatting

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -12,41 +12,45 @@ namespace SecaFolderWatcher.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private string _InfoText = "";
-        public string InfoText {
-          get { return _InfoText; }
-          set { this.RaiseAndSetIfChanged(ref _InfoText, value); }
+        public string InfoText
+        {
+            get { return _InfoText; }
+            set { this.RaiseAndSetIfChanged(ref _InfoText, value); }
         }
 
-        public ICommand DialogWindowCommand {get;}
+        public ICommand DialogWindowCommand { get; }
         public Interaction<DialogWindowViewModel, DialogResultViewModel?> ShowDialog { get; }
 
 
-    public void Click()
-    {
-      InfoText = InfoText + "test\n";
-      Logger.LogInformation("Clicked Button HCHS");
-    }
-    public GetPatientDataViewModel PatientData { get; } = new GetPatientDataViewModel();
+        public void Click()
+        {
+            InfoText = InfoText + "test\n";
+            Logger.LogInformation("Clicked Button HCHS");
+        }
+        public GetPatientDataViewModel PatientData { get; } = new GetPatientDataViewModel();
 
-    public MainWindowViewModel()
-    {
-      Logger.RegisterOnLogCallback(delegate () {
-          InfoText = Logger.GetSessionLog();
-          });
-      try
-      {
-        SettingsReader.InitSettingsReader();
-        Logger.SetLogPath(SettingsReader.GetFilePathOf("LOGFILE"));
-      }
-      catch (Exception e){
-        Logger.LogError($"There has been an error while trying to process the program settings. The provided error message is \n {e.Message}");
-      }
-      ShowDialog = new Interaction<DialogWindowViewModel,DialogResultViewModel?>();
-      DialogWindowCommand = ReactiveCommand.CreateFromTask(async () => {
-          var dialog = new DialogWindowViewModel();
-          var result = await ShowDialog.Handle(dialog);
-          });
-      GDT_Content gdt = new GDT_Content("./testing/mddtseca.gdt");
+        public MainWindowViewModel()
+        {
+            Logger.RegisterOnLogCallback(delegate ()
+            {
+                InfoText = Logger.GetSessionLog();
+            });
+            try
+            {
+                SettingsReader.InitSettingsReader();
+                Logger.SetLogPath(SettingsReader.GetFilePathOf("LOGFILE"));
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"There has been an error while trying to process the program settings. The provided error message is \n {e.Message}");
+            }
+            ShowDialog = new Interaction<DialogWindowViewModel, DialogResultViewModel?>();
+            DialogWindowCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var dialog = new DialogWindowViewModel();
+                var result = await ShowDialog.Handle(dialog);
+            });
+            GDT_Content gdt = new GDT_Content("./testing/mddtseca.gdt");
+        }
     }
-}
 }
