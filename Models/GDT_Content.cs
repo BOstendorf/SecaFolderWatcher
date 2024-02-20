449cee014d5bfb8b9ac767f7b24eb621c26cbee9
benjamin <benjamin.ostendorf@gmx.de>
2024-02-20 17:24:04 +0100

    add additional parameter for file name and overload function that defaults filename to secamddt.gdt

--- Models/GDT_Content.cs
+++ Models/GDT_Content.cs
@@ -97,11 +97,15 @@ public class GDT_Content
     }
   }
 
+  public static void SendPatientToWatchfolder(string targetDirPath, string dhcc, string dateOfBirth, string sex){
+    SendPatientToWatchfolder(targetDirPath, "secamddt.gdt", dhcc, dateOfBirth, sex);
+  }
+
   /*
    * Function to create a GDT message containing the patient id, the date of birth and the gender.
    * The message should be created as a file in the specified watchfolder
    */
-  public static void SendPatientToWatchfolder(string targetPath, string dhcc, string dateOfBirth, string sex)
+  public static void SendPatientToWatchfolder(string targetDirPath, string targetFileName, string dhcc, string dateOfBirth, string sex)
   {
     if (!DataValidator.CheckSex(sex)) {
       throw new ArgumentException($"{DataValidator.GetSexFormatDescription()} The actual value is {sex}");
